5568109cc450542b561ee7db39ae1156670aa121
Charles Uthulor <charlesnnanna1667@gmail.com>
2024-07-15 14:31:45 +0100

    Made some changes

---
+++ components/HomeComponents/Chapter.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import HomeList from "./HomeList";
+
+export default function Chapter() {
+  return (
+    <div className="max-w-[700px] border border-black p-6 shadow-xl">
+      <h3 className="text-xl mb-4">Chapter-01 Get Started Intro</h3>
+      <p className="mb-2">
+        Making this the first true value generator on the Internet. It of over
+        200 Latin words, combined with a handful.
+      </p>
+      <div className="flex flex-row">
+        <HomeList title="Pages" value="185 pages" />
+        <HomeList title="Lenght" value="1 hour 16 mins" />
+      </div>
+    </div>
+  );
+}
--- components/HomeComponents/Chapters.tsx
+++ components/HomeComponents/Chapters.tsx
@@ -5,6 +5,8 @@ import {
   BoltIcon,
   ScaleIcon,
 } from "@heroicons/react/24/outline";
+import SectionHeader from "../SectionHeader";
+import Chapter from "./Chapter";
 
 const features = [
   {
@@ -35,37 +37,14 @@ const features = [
 
 export default function Chapters() {
   return (
-    <div className="py-12 bg-white">
+    <div className="py-12 bg-neutral border border-black text-primary">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="lg:text-center">
-          <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">
-            Transactions
-          </h2>
-          <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-            A better way to send money
-          </p>
-          <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
-            Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam
-            voluptatum cupiditate veritatis in accusamus quisquam.
-          </p>
-        </div>
+        <SectionHeader style="items-center" title="The Chapter Includes" />
 
         <div className="mt-10">
-          <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
-            {features.map((feature) => (
-              <div key={feature.name} className="relative">
-                <dt>
-                  <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                    <feature.icon className="h-6 w-6" aria-hidden="true" />
-                  </div>
-                  <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
-                    {feature.name}
-                  </p>
-                </dt>
-                <dd className="mt-2 ml-16 text-base text-gray-500">
-                  {feature.description}
-                </dd>
-              </div>
+          <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-6 ">
+            {features.map((feature, index) => (
+              <Chapter key={index} />
             ))}
           </dl>
         </div>
